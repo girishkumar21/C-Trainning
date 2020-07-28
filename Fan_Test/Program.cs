862682e5f4365b593376cba8d94c850d05486048
girishkumar21 <girishkumar538@gmail.com>
2020-07-28 17:41:51 +0530

    first Commit

---
+++ Fan_Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fan_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (ManagementObject Fan in new ManagementObjectSearcher(
+                "select SystemName from Win32_Fan").Get())
+            {
+
+                Console.WriteLine("Device Name:: " + Fan["SystemName"]);
+            }
+
+            Console.WriteLine("Press any to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IRQ_Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRQ_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (ManagementObject Memory in new ManagementObjectSearcher(
+                "select * from Win32_IRQResource").Get())
+            {
+                Console.WriteLine("Name:: " + Memory["Name"]);
+                Console.WriteLine("Status:: " + Memory["Status"]);
+                Console.WriteLine("IRQNumber:: " + Memory["IRQNumber"]);
+                Console.WriteLine("Device Name:: " + Memory["CSName"]);
+                Console.WriteLine("-----------------------------------");
+
+            }
+
+            Console.WriteLine("Press Any Key to Exit");
+            Console.ReadKey();
+        }
+    }
+}
